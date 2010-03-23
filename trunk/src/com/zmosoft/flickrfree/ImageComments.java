d0681814c472c6e0716572783760b88a262c1c34
rnstewart <rnstewart@96173220-73de-ef7c-72b6-e49ec6cc2051>
2010-03-23 06:58:05 +0000

    1) Consolidated all API calls into APICalls class.
    2) Updated handling of URLs in image comments. This is in preparation for
    making them clickable elements that will take the user to the appropriate
    group or image within FlickrFree.

--- trunk/src/com/zmosoft/flickrfree/ImageComments.java
+++ trunk/src/com/zmosoft/flickrfree/ImageComments.java
@@ -1,5 +1,7 @@
 package com.zmosoft.flickrfree;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.LinkedHashMap;
 
 import org.json.JSONArray;
@@ -21,16 +23,10 @@ public class ImageComments extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.imagecomments);
         m_extras = getIntent().getExtras();
-        m_comment_list = GetCommentList();
+        m_comment_list = APICalls.photosCommentsGetList(m_extras.getString("photo_id"));
         FillTable();
     }
 
-    private JSONObject GetCommentList() {
-    	String[] paramNames = {"photo_id"};
-        String[] paramVals = {m_extras.getString("photo_id")};
-        return RestClient.CallFunction("flickr.photos.comments.getList",paramNames,paramVals);
-    }
-    
     private void FillTable() {
     	try {
 			LinkedHashMap<String,String> comments = new LinkedHashMap<String,String>();
@@ -49,16 +45,26 @@ public class ImageComments extends Activity implements OnClickListener {
 				}
 			}
 
-			View entry;
+			CommentLayout entry;
+			String comment;
+			CharSequence formatted_comment;
 			for (String key : comments.keySet()) {
 				// Add the title/value entry pair for the set of comments.
-				entry = View.inflate(this, R.layout.image_comment_entry, null);
+				entry = (CommentLayout)View.inflate(this, R.layout.image_comment_entry, null);
 				((TextView)entry.findViewById(R.id.Author)).setText(key);
 				
+				comment = comments.get(key);
+				if (ReadLinksInComment(entry, comment)) {
+					// Set the "Go To" button to visible and make it a link
+					// to go to the given group page.
+				}
+				
+				//comment = ConvertURLstoURIs(comment);
 				// The comment might have HTML tags, so use the Html class to handle
 				// that.
-				CharSequence fString = Html.fromHtml(comments.get(key),null,null);
-				((TextView)entry.findViewById(R.id.Comment)).setText(fString);
+				formatted_comment = Html.fromHtml(comments.get(key),null,null);
+				((TextView)entry.findViewById(R.id.Comment)).setText(formatted_comment);
+				
 				
 				//((TextView)entry.findViewById(R.id.InfoValue)).setClickable(true);
 				//((TextView)entry.findViewById(R.id.InfoValue)).setOnClickListener(this);
@@ -66,9 +72,110 @@ public class ImageComments extends Activity implements OnClickListener {
 				((LinearLayout)findViewById(R.id.ImgCommentsLayout)).addView(entry);
 			}
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    }
+    
+    public boolean ReadLinksInComment(CommentLayout entry, String comment) {
+    	int links_found = 0;
+    	
+    	Integer[] url_pos = findURLPosInComment(comment, 0);
+    	
+    	while (url_pos != null) {
+    		++links_found;
+			String url_str = comment.substring(url_pos[0], url_pos[1]);
+			URL url;
+    		try {
+    			if (url_str.substring(0, 1).equals("/")) {
+    				// If the URL starts with "/", then it is probably a relative URL.
+    				// We need to prepend the rest of the Flickr URL onto it so it can
+    				// be read correctly.
+    				url_str = "http://www.flickr.com" + url_str;
+    			}
+    			else if (url_str.substring(0, 3).equals("www")) {
+    				url_str = "http://" + url_str;
+    			}
+    			url = new URL(url_str);
+			} catch (MalformedURLException e) {
+		    	url_pos = findURLPosInComment(comment, url_pos[1] + 1);
+				continue;
+			}
+			String path = url.getPath();
+			if (url.getProtocol().equals("http")) {
+				String id;
+				if (path.contains("groups")) {
+					id = path.substring(path.indexOf("groups/") + 7);
+					if (id.contains("/")) {
+						id = id.substring(0, id.indexOf("/"));
+					}
+					// TODO Put code in here for getting the group name from the id.
+					// Or, alternatively, if the group URL only gives the name, then
+					// get the id.
+					entry.m_group_links.put("name", id);
+				}
+				else if (path.contains("photo")) {
+					id = path.substring(path.indexOf("photo/") + 6);
+					if (id.contains("/")) {
+						id = id.substring(0, id.indexOf("/"));
+					}
+					// TODO Put code in here for getting the photo name from the id.
+					entry.m_photo_links.put("name", id);
+				}
+			}
+	    	url_pos = findURLPosInComment(comment, url_pos[1] + 1);
+    	}
+    	
+    	return (links_found > 0);
+    }
+    
+    public String ConvertURLstoURIs(String comment) {
+    	Integer[] url_pos = findURLPosInComment(comment, 0);
+    	
+    	while (url_pos != null) {
+			String url_str = comment.substring(url_pos[0], url_pos[1]);
+			URL url;
+    		try {
+    			if (url_str.substring(0, 1).equals("/")) {
+    				// If the URL starts with "/", then it is probably a relative URL.
+    				// We need to prepend the rest of the Flickr URL onto it so it can
+    				// be read correctly.
+    				url_str = "http://www.flickr.com" + url_str;
+    			}
+    			url = new URL(url_str);
+			} catch (MalformedURLException e) {
+		    	url_pos = findURLPosInComment(comment, url_pos[1] + 1);
+				continue;
+			}
+			String path = url.getPath();
+			if (url.getProtocol().equals("http") && path.contains("groups")) {
+				String group_id = path.substring(path.indexOf("groups/") + 7);
+				if (group_id.contains("/")) {
+					group_id = group_id.substring(0, group_id.indexOf("/"));
+				}
+				String uri = "flickr://flickrfree/?group_id=" + group_id;
+				comment = comment.substring(0, url_pos[0]) + uri + comment.substring(url_pos[1]);
+			}
+	    	url_pos = findURLPosInComment(comment, url_pos[1] + 1);
+    	}
+    	
+    	return comment;
+    }
+    
+    Integer[] findURLPosInComment(String comment) {
+    	return findURLPosInComment(comment, 0);
+    }
+    
+    Integer[] findURLPosInComment(String comment, int start) {
+    	int href_start = comment.indexOf("<a href=\"", start);
+    	int href_end = comment.indexOf("\">", href_start);
+    	Integer[] range = null;
+    	
+    	if (href_start >= 0) {
+    		href_start += 9;
+    		range = new Integer[]{href_start, href_end};
+    	}
+    	
+    	return range;
     }
     
 	@Override
@@ -86,7 +193,6 @@ public class ImageComments extends Activity implements OnClickListener {
 //					e.printStackTrace();
 //				}
 //			} catch (JSONException e) {
-//				// TODO Auto-generated catch block
 //				e.printStackTrace();
 //			}
 //		}
