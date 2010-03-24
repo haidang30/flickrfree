2350e480347c1152c32f27c35596cef997d0dc5b
rnstewart <rnstewart@96173220-73de-ef7c-72b6-e49ec6cc2051>
2010-03-24 04:30:14 +0000

    First elements of what will become the "Go To..." view for comment links.

---
+++ src/com/zmosoft/flickrfree/CommentLinkView.java
@@ -0,0 +1,22 @@
+package com.zmosoft.flickrfree;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class CommentLinkView extends Activity implements OnClickListener {
+
+	public CommentLinkView() {
+	}
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comment_link_view);
+    }
+    
+	@Override
+	public void onClick(View v) {
+	}
+
+}
