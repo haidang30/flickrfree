ab8868afb9d6d4e0618d3c10e7f24fe63154fe56
rnstewart <rnstewart@96173220-73de-ef7c-72b6-e49ec6cc2051>
2010-03-28 02:51:02 +0000

    Integrated JSONParser into ImageInfo.

--- src/com/zmosoft/flickrfree/ImageInfo.java
+++ src/com/zmosoft/flickrfree/ImageInfo.java
@@ -41,81 +41,68 @@ public class ImageInfo extends Activity implements OnClickListener {
 			LinkedHashMap<String,String> info = new LinkedHashMap<String,String>();
 			HashMap<String,String> exif = new LinkedHashMap<String,String>();
 			
-			if (m_exif.has("photo") && m_exif.getJSONObject("photo").has("exif")) {
-				JSONArray exif_arr = m_exif.getJSONObject("photo").getJSONArray("exif");
-				JSONObject exif_entry;
-				for (int i = 0; i < exif_arr.length(); i++) {
-					exif_entry = null;
-					exif_entry = exif_arr.getJSONObject(i);
-					if (exif_entry != null && exif_entry.has("label") && exif_entry.has("raw") && exif_entry.getJSONObject("raw").has("_content")) {
-						exif.put(exif_entry.getString("label"), exif_entry.getJSONObject("raw").getString("_content"));
-					}
-				}
+			JSONArray exif_arr = JSONParser.getArray(m_exif, "photo/exif");
+			JSONObject exif_entry;
+			for (int i = 0; exif_arr != null && i < exif_arr.length(); i++) {
+				exif_entry = null;
+				exif_entry = exif_arr.getJSONObject(i);
+				exif.put(JSONParser.getString(exif_entry, "label"), JSONParser.getString(exif_entry, "raw/_content"));
 			}
 
-			if (m_imginfo.has("photo")) {
-				JSONObject photo = m_imginfo.getJSONObject("photo");
-				if (photo.has("owner")) {
-					JSONObject owner = photo.getJSONObject("owner");
-					String owner_str = "";
-					if (owner.has("realname") && !owner.getString("realname").equals("")) {
-						owner_str += owner.getString("realname");
-						if (owner.has("username") && !owner.getString("username").equals("")) {
-							owner_str += " (" + owner.getString("username") + ")";
-						}
-					}
-					else if (owner.has("username") && !owner.getString("username").equals("")) {
-						owner_str = owner.getString("username");
-					}
-					if (!owner_str.equals("")) {
-						info.put(getResources().getString(R.string.imageinfo_owner),owner_str);
-					}
-
-					if (owner.has("location") && !owner.getString("location").equals("")) {
-						info.put(getResources().getString(R.string.imageinfo_owner_location),owner.getString("location"));
-					}
+			JSONObject owner = JSONParser.getObject(m_imginfo, "photo/owner");
+			String realname = JSONParser.getString(owner, "realname");
+			String username = JSONParser.getString(owner, "username");
+			String owner_str = "";
+			if (realname != null && username != null) {
+				owner_str = username;
+				if (!realname.equals("")) {
+					owner_str = realname + " (" + username + ")";
 				}
+			}
+			
+			if (!owner_str.equals("")) {
+				info.put(getResources().getString(R.string.imageinfo_owner),owner_str);
+			}
 
-				if (photo.has("dates")) {
-					JSONObject dates = photo.getJSONObject("dates");
-					if (dates.has("taken") && !dates.getString("taken").equals("")) {
-						info.put(getResources().getString(R.string.imageinfo_datetaken),dates.getString("taken"));
-					}
+			String location = JSONParser.getString(owner, "location");
+			if (!location.equals("")) {
+				info.put(getResources().getString(R.string.imageinfo_owner_location),location);
+			}
+
+			String date_taken = JSONParser.getString(m_imginfo, "photo/dates/taken");
+			if (date_taken != null) {
+				info.put(getResources().getString(R.string.imageinfo_datetaken),date_taken);
+			}
+
+			if (exif.containsKey("GPS Latitude") && exif.containsKey("GPS Longitude")) {
+				String latitude = String.valueOf(GlobalResources.LatLongToDecimal(exif.get("GPS Latitude")));
+				String longitude = String.valueOf(GlobalResources.LatLongToDecimal(exif.get("GPS Longitude")));
+				if (exif.containsKey("GPSLatitudeRef") && exif.containsKey("GPSLongitudeRef")) {
+					latitude += exif.get("GPSLatitudeRef").substring(0, 1); 
+					longitude += exif.get("GPSLongitudeRef").substring(0, 1); 
 				}
-
-				if (exif.containsKey("GPS Latitude") && exif.containsKey("GPS Longitude")) {
-					String latitude = String.valueOf(GlobalResources.LatLongToDecimal(exif.get("GPS Latitude")));
-					String longitude = String.valueOf(GlobalResources.LatLongToDecimal(exif.get("GPS Longitude")));
-					if (exif.containsKey("GPSLatitudeRef") && exif.containsKey("GPSLongitudeRef")) {
-						latitude += exif.get("GPSLatitudeRef").substring(0, 1); 
-						longitude += exif.get("GPSLongitudeRef").substring(0, 1); 
-					}
+				info.put(getResources().getString(R.string.imageinfo_locationtaken),
+						 latitude + ", " + longitude);
+			}
+			else {
+				JSONObject latitude = JSONParser.getObject(m_imginfo, "photo/location/latitude");
+				JSONObject longitude = JSONParser.getObject(m_imginfo, "photo/location/longitude");
+				if (latitude != null && longitude != null) {
 					info.put(getResources().getString(R.string.imageinfo_locationtaken),
 							 latitude + ", " + longitude);
 				}
-				else if (photo.has("location")) {
-					JSONObject location = photo.getJSONObject("location");
-					if (location.has("latitude") && location.has("longitude")
-						&& !location.getString("latitude").equals("")
-						&& !location.getString("longitude").equals("")) {
-						info.put(getResources().getString(R.string.imageinfo_locationtaken),
-								location.getString("latitude") + ", " + location.getString("longitude"));
-					}
-				}
-				
-				String camera = (exif.containsKey("Make") ? exif.get("Make") : "")
-							+	(exif.containsKey("Model") ? " " + exif.get("Model") : "");
-				if (!camera.equals("")) {
-					info.put(getResources().getString(R.string.imageinfo_camera), camera);
-				}
-				
-				if (photo.has("description")) {
-					JSONObject t = photo.getJSONObject("description");
-					if (t.has("_content") && !t.getString("_content").equals("")) {
-						info.put(getResources().getString(R.string.imageinfo_description), t.getString("_content"));
-					}
-				}
-	    	}
+			}
+			
+			String camera = (exif.containsKey("Make") ? exif.get("Make") : "")
+						+	(exif.containsKey("Model") ? " " + exif.get("Model") : "");
+			if (!camera.equals("")) {
+				info.put(getResources().getString(R.string.imageinfo_camera), camera);
+			}
+			
+			String description = JSONParser.getString(m_imginfo, "photo/description/_content");
+			if (description != null && !description.equals("")) {
+				info.put(getResources().getString(R.string.imageinfo_description), description);
+			}
 	
 			View entry;
 			Button entry_button;
@@ -154,7 +141,7 @@ public class ImageInfo extends Activity implements OnClickListener {
 		if (v.getId() == R.id.btnImageInfo) {
 			if (((Button)v).getText().equals(getResources().getString(R.string.btnuserpagelabel))) {
 				try {
-					String username = m_imginfo.getJSONObject("photo").getJSONObject("owner").getString("username");
+					String username = JSONParser.getString(m_imginfo, "photo/owner/username");
 					String nsid = APICalls.getNSIDFromName(username);
 		
 					Intent i = new Intent(this, UserView.class);
