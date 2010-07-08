f0d1e92526fbc6f0cb069fcb31b958e5b416ae9a
rnstewart <rnstewart@96173220-73de-ef7c-72b6-e49ec6cc2051>
2010-07-08 18:44:36 +0000

    Improvements to uploader status-bar notification.

--- src/com/zmosoft/flickrfree/Uploader.java
+++ src/com/zmosoft/flickrfree/Uploader.java
@@ -7,30 +7,43 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 
 public class Uploader extends Service {
 
 	// AsyncTask to upload a picture in the background.
-	private class UploadPicture extends AsyncTask<Bundle, Void, Object> {
+	private class UploadPictureTask extends AsyncTask<Bundle, String, Object> {
 		
 		@Override
 		protected Object doInBackground(Bundle... params) {
-			Bundle upload_info = params.length > 0 ? params[0] : null;
+			m_upload_info = params.length > 0 ? params[0] : null;
 			
-			if (upload_info != null) {
-		        RestClient.UploadPicture(upload_info.getString("filename"),
-						    			 upload_info.getString("title"),
-							    		 upload_info.getString("comment"),
-							    		 upload_info.getString("tags"),
-							    		 upload_info.getBoolean("is_public"),
-							    		 upload_info.getBoolean("is_friend"),
-							    		 upload_info.getBoolean("is_family"),
-							    		 upload_info.getInt("safety_level"));
+			if (m_upload_info != null) {
+				publishProgress(m_upload_info.getString("title"));
+				
+		        RestClient.UploadPicture(m_upload_info.getString("filename"),
+						    			 m_upload_info.getString("title"),
+							    		 m_upload_info.getString("comment"),
+							    		 m_upload_info.getString("tags"),
+							    		 m_upload_info.getBoolean("is_public"),
+							    		 m_upload_info.getBoolean("is_friend"),
+							    		 m_upload_info.getBoolean("is_family"),
+							    		 m_upload_info.getInt("safety_level"));
 			}
 			
 			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(String... progress) {
+			String title = progress.length > 0 ? progress[0] : "";
+			m_notification.setLatestEventInfo(getApplicationContext(),
+					  getApplicationContext().getString(R.string.app_name),
+					  getApplicationContext().getString(R.string.uploadingpicture) + " \"" + title + "\"",
+					  m_notify_activity);
+			((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE)).notify(GlobalResources.UPLOADER_ID, m_notification);
 		}
 		
 		@Override
@@ -45,8 +58,20 @@ public class Uploader extends Service {
 			((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE)).cancel(GlobalResources.UPLOADER_ID);
 			stopSelf();
 		}
+		
+		public Bundle getUploadInfo() {
+			return m_upload_info;
+		}
+		
+		Bundle m_upload_info = null;
 	}
 	
+	public class UploadBinder extends Binder {
+        Uploader getService() {
+            return Uploader.this;
+        }
+    }
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -56,28 +81,40 @@ public class Uploader extends Service {
 	public void onStart(Intent intent, int startId) {
 		
 		int icon = android.R.drawable.stat_sys_upload;
-		CharSequence tickerText = "FlickrFree Uploading Picture";
-		long when = System.currentTimeMillis();
-
-		m_notification = new Notification(icon, tickerText, when);
+		CharSequence tickerText = "Uploading Picture";
+		m_notification = new Notification(icon, tickerText, System.currentTimeMillis());
+		m_notify_activity = PendingIntent.getActivity(this, 0, new Intent(this, Uploader.class), 0);
 		
-		Context context = getApplicationContext();
-		CharSequence contentTitle = "FlickrFree";
-		CharSequence contentText = "Uploading Picture";
-		Intent notificationIntent = new Intent(this, Uploader.class);
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 		if (m_notification != null) {
-			m_notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+			m_notification.setLatestEventInfo(getApplicationContext(),
+											  this.getString(R.string.app_name),
+											  this.getString(R.string.uploadingpicture),
+											  m_notify_activity);
 			m_notification.flags = Notification.FLAG_NO_CLEAR;
 		}
 		
-		new UploadPicture().execute(intent.getExtras());
+		m_upload_task = (UploadPictureTask)new UploadPictureTask().execute(intent.getExtras());
 	}
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
-		return null;
+		return m_binder;
 	}
 
-	Notification m_notification = null;
+	public String getFilename() {
+		String rval = "";
+		if (m_upload_task != null) {
+			Bundle upload_info = m_upload_task.getUploadInfo();
+			if (upload_info != null) {
+				rval = upload_info.getString("filename");
+			}
+		}
+		
+		return rval;
+	}
+	
+	private Notification m_notification = null;
+	private PendingIntent m_notify_activity = null;
+	private final IBinder m_binder = new UploadBinder();
+	private UploadPictureTask m_upload_task = null;
 }
