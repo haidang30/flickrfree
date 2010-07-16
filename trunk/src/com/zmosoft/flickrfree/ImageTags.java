f173e313c2c04b9777099be16db3784d1aec0f9e
rnstewart <rnstewart@96173220-73de-ef7c-72b6-e49ec6cc2051>
2010-07-16 03:58:44 +0000

    Added some more functionality to some of the Flickr API call convenience functions.

--- trunk/src/com/zmosoft/flickrfree/ImageTags.java
+++ trunk/src/com/zmosoft/flickrfree/ImageTags.java
@@ -40,7 +40,7 @@ public class ImageTags extends Activity implements OnItemClickListener {
 				int i = 0, nphotos = 0;
 				while (itr.hasNext() && !isCancelled()) {
 					tag = itr.next();
-					photolist = APICalls.photosSearch(m_nsid, tag);
+					photolist = APICalls.photosSearch(m_nsid, null, tag);
 					if (photolist != null) {
 						m_tagmap.put(tag, photolist);
 						n_completed += 1;
